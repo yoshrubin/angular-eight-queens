5cadcc964ca50a956be137b76c1188c9f6ab85e8
Yoshi Rubin <yoshrubin@gmail.com>
2021-01-23 21:58:13 +0200

    the board is generating

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
+
 export class GameBoardComponent implements OnInit {
+
 
   constructor() { }
 
   ngOnInit(): void {
+    const board = document.getElementById('board');
+    this.renderBoard(board);
+  }
+
+  renderBoard(board: any) {
+    for (var i = 0; i < 8; ++i) {
+      var row = document.createElement('DIV')
+      row.className = 'row'
+      row.style.display = 'flex';
+      row.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse';
+      for (var j = 0; j < 8; ++j) {
+        var square = document.createElement('DIV')
+        square.className = 'square'
+        square.style.backgroundColor = j % 2 === 0 ? 'white' : 'black'
+        square.style.height = '100px'
+        square.style.width = '100px'
+        row.appendChild(square)
+      }
+      board.appendChild(row)
+    }
   }
 
 }
