578e96ae0184031e8ce9615032acf7e4915feb43
Yoshi Rubin <yoshrubin@gmail.com>
2021-01-24 22:02:07 +0200

    https://github.com/yoshrubin/angular-eight-queens/issues/1 fixed the board so the arrays work better

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -8,30 +8,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class GameBoardComponent implements OnInit {
 
+  rows = new Array(8);
+  squares = new Array(8);
 
   constructor() { }
 
   ngOnInit(): void {
-    const board = document.getElementById('board');
-    this.renderBoard(board);
   }
 
-  renderBoard(board: any) {
-    for (var i = 0; i < 8; ++i) {
-      var row = document.createElement('DIV')
-      row.className = 'row'
-      row.style.display = 'flex';
-      row.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse';
-      for (var j = 0; j < 8; ++j) {
-        var square = document.createElement('DIV')
-        square.className = 'square'
-        square.style.backgroundColor = j % 2 === 0 ? 'white' : 'black'
-        square.style.height = '100px'
-        square.style.width = '100px'
-        row.appendChild(square)
-      }
-      board.appendChild(row)
-    }
+  toggleQueen(row: any, square: any) {
+    console.log(row, square);
   }
 
 }
