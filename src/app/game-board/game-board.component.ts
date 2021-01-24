03f32eeef0b82bbab5dcf573253cdc03cff8fa5d
Yoshi Rubin <yoshrubin@gmail.com>
2021-01-24 23:13:54 +0200

    https://github.com/yoshrubin/angular-eight-queens/issues/2 will block from inserting queen in row or column

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -8,16 +8,37 @@ import { Component, OnInit } from '@angular/core';
 
 export class GameBoardComponent implements OnInit {
 
+  queens: number[][] = new Array(8)
+    .fill(0)
+    .map(() => new Array(8)
+      .fill(0)
+    );
   rows = new Array(8);
   squares = new Array(8);
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.queens);
   }
 
-  toggleQueen(row: any, square: any) {
-    console.log(row, square);
+  toggleQueen(row: any, column: any) {
+    for (var x in this.queens) {
+      for (var y in this.queens[x]) {
+        console.log(this.queens[x][y],'value');
+        console.log(x,'x',row);
+        console.log(y,'y',column);
+        if (this.queens[x][y] && x == row) {
+          console.log('cannot place queen in this row');
+          return;
+        }
+        if (this.queens[x][y] && y == column) {
+          console.log('cannot place queen in this column');
+          return;
+        }
+      }
+    }
+    this.queens[row][column] = 1;
   }
 
 }
